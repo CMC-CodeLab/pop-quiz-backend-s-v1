4731543377731e864540015fde55e47273b8f9d7
Thai Nguyen Duy <thaind81@gmail.com>
2023-03-13 09:16:04 +0700

    going to work

---
+++ db/Dump20230313.sql
@@ -0,0 +1,144 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: regov_school
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `course`
+--
+
+DROP TABLE IF EXISTS `course`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `course` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `course_name` varchar(255) NOT NULL,
+  `number_of_students_enrolled` int NOT NULL DEFAULT '0',
+  `seats_left` int NOT NULL,
+  `maximum_capacity` int NOT NULL,
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  `created_by` int NOT NULL,
+  `userId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_bceb52bbd16679020822f6d6f5d` (`userId`),
+  CONSTRAINT `FK_bceb52bbd16679020822f6d6f5d` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `course`
+--
+
+LOCK TABLES `course` WRITE;
+/*!40000 ALTER TABLE `course` DISABLE KEYS */;
+INSERT INTO `course` VALUES (1,'Class 1',8,2,17,'2023-03-13 00:17:46.132000','2023-03-13 01:35:24.000000',0,NULL),(2,'Class 2',0,15,15,'2023-03-13 00:35:01.372998','2023-03-13 00:35:01.372998',0,NULL),(3,'Class 3',0,20,20,'2023-03-13 00:36:32.734071','2023-03-13 00:36:32.734071',0,NULL),(4,'Class 3',0,20,20,'2023-03-13 07:52:08.510449','2023-03-13 07:52:08.510449',1,NULL),(5,'Class 3',0,20,20,'2023-03-13 07:53:27.587721','2023-03-13 07:53:27.587721',1,NULL),(6,'Class 3',0,20,20,'2023-03-13 07:54:52.484414','2023-03-13 07:54:52.484414',1,NULL),(7,'Class 3',0,20,20,'2023-03-13 08:22:10.350771','2023-03-13 08:22:10.350771',1,NULL);
+/*!40000 ALTER TABLE `course` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `course_registration_history`
+--
+
+DROP TABLE IF EXISTS `course_registration_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `course_registration_history` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `course_name` varchar(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `status` enum('unregistered','registered') NOT NULL DEFAULT 'registered',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `course_registration_history`
+--
+
+LOCK TABLES `course_registration_history` WRITE;
+/*!40000 ALTER TABLE `course_registration_history` DISABLE KEYS */;
+INSERT INTO `course_registration_history` VALUES (1,'2023-03-13 04:07:54.268468','Class 1','ndthai1','registered'),(2,'2023-03-13 07:28:08.338887','Class 1','ndthai1','registered'),(3,'2023-03-13 07:29:00.736930','Class 1','ndthai1','registered'),(4,'2023-03-13 08:21:34.905753','Class 1','ndthai1','registered'),(5,'2023-03-13 09:06:54.017271','Class 1','ndthai1','registered');
+/*!40000 ALTER TABLE `course_registration_history` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `courses_students`
+--
+
+DROP TABLE IF EXISTS `courses_students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `courses_students` (
+  `course_id` int NOT NULL,
+  `student_id` int NOT NULL,
+  `created_date` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  PRIMARY KEY (`course_id`,`student_id`),
+  KEY `IDX_373fad2fd5b550031938f5afe1` (`course_id`),
+  KEY `IDX_38ccc15459cb8777aadacbf7dc` (`student_id`),
+  CONSTRAINT `FK_373fad2fd5b550031938f5afe1f` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_38ccc15459cb8777aadacbf7dc1` FOREIGN KEY (`student_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `courses_students`
+--
+
+LOCK TABLES `courses_students` WRITE;
+/*!40000 ALTER TABLE `courses_students` DISABLE KEYS */;
+INSERT INTO `courses_students` VALUES (1,1,'2023-03-13 09:07:42.406253');
+/*!40000 ALTER TABLE `courses_students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `roles` set('Administrator','Student') NOT NULL DEFAULT 'Student',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Thai Nguyen Duy','ndthai1','$2b$10$VNZZc.kDAE7.9jlVDfDsaeTq.Wysa0FOcFtDWu1JJE0GCED.kXhGa','ndthai1@cmcglobal.vn','Student');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-13  9:15:33
